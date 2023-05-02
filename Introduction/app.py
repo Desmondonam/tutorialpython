7165bf2de6094a6f0bb3b553bfa1156f2da47627
Desmondonam <desmondonam@gmail.com>
2023-05-02 20:38:26 +0300

    add files

---
+++ Introduction/app.py
@@ -0,0 +1,20 @@
+# print("Hello Desmond!")
+# print("0----")
+# print(" ||||")
+
+# print("*"*10)
+
+'''
+Variables in python
+'''
+# price = 10
+# print(price)
+
+'''
+programe to take in a patients name age and status if new
+'''
+name = 'Desmond Onam'
+age = 24
+is_new = False
+
+print(f'{name} of {age} years old is new')
